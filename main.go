aa8d4e135b6e7e66774ac6f38d0e5f934b699ec6
John Gosset <john@gossetx.com>
2016-10-16 15:40:38 -0400

    Refactoring for clarity

--- main.go
+++ main.go
@@ -15,33 +15,35 @@ import (
 var (
 	FCtl = "/sys/class/backlight/intel_backlight/brightness"
 	FMax = "/sys/class/backlight/intel_backlight/max_brightness"
-
-	// Maximum brightness (float64).
-	Max = Get(FMax)
 )
 
 // Get retrieves a brightness value from the provided control file. Result
-// is a float64 representing the brightness in arbitrary "brightness units"
-// relative to Max.
-func Get(fctl string) float64 {
+// is a float64 representing the brightness in arbitrary "brightness units".
+func Get(fctl string) (float64, error) {
 	var brightness float64
 
 	b, err := ioutil.ReadFile(fctl)
-	check(err)
+	if err != nil {
+		return brightness, err
+	}
 
 	s := strings.TrimSpace(string(b))
 	brightness, err = strconv.ParseFloat(s, 64)
-	check(err)
+	if err != nil {
+		return brightness, err
+	}
 
-	return brightness
+	return brightness, nil
 }
 
 // Set writes backlight brightness to the provided control file.
-// levelpct: overall brightness level expressed as a percentage (ex: 30 -> 30% of max brightness)
-func Set(fctl string, pct float64) {
-	b := strconv.AppendFloat([]byte(nil), FromPct(pct, Max), 'f', 0, 64)
-	err := ioutil.WriteFile(fctl, b, 0644)
-	check(err)
+// pct: overall brightness level expressed as a percentage (ex: 30 -> 30% of max brightness)
+func Set(fctl string, pct, max float64) error {
+	b := strconv.AppendFloat([]byte(nil), FromPct(pct, max), 'f', 0, 64)
+	if err := ioutil.WriteFile(fctl, b, 0644); err != nil {
+		return err
+	}
+	return nil
 }
 
 // ToPct returns a percent value for brightness, given a brightness and
@@ -64,18 +66,33 @@ func main() {
 	}
 	flag.Parse()
 
-	if len(os.Args) != 2 {
-		fmt.Printf("%0.0f\n", ToPct(Get(FCtl), Max))
-		os.Exit(0)
+	// Get maximum brightness.
+	max, err := Get(FMax)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	level, err := strconv.ParseFloat(os.Args[1], 64)
-	check(err)
-	Set(FCtl, level)
-}
+	switch flag.NArg() {
 
-func check(e error) {
-	if e != nil {
-		log.Fatal(e)
+	// Print current brightness to stdout.
+	case 0:
+		b, err := Get(FCtl)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%0.0f\n", ToPct(b, max))
+
+	// Set brightness to provided pct value.
+	case 1:
+		level, err := strconv.ParseFloat(os.Args[1], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := Set(FCtl, level, max); err != nil {
+			log.Fatal(err)
+		}
+
+	default:
+		flag.Usage()
 	}
 }
